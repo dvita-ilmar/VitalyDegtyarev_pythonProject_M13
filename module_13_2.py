fc2a4561182bba4132443840a43a21c13521e1ac
dvita-ilmar <dvita@mail.ru>
2024-12-12 10:22:09 +0500

    module_13_2.py is completed

---
+++ module_13_2.py
@@ -0,0 +1,33 @@
+"""
+coding: utf-8
+Дегтярев Виталий (группа 22/08)
+Домашнее задание №13.2
+Домашнее задание по теме "Хендлеры обработки сообщений".
+"""
+
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+
+#@dp.message_handler(text=["Dantes"])
+#async def dantes_messages(message):
+#    print("Dantes message")
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    print("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message_handler()
+async def all_message(message):
+    print("Введите команду /start, чтобы начать общение.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
